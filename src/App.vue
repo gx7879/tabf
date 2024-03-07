74e4702cbf72d2019f8c6d1d845354c36da4dc9b
吳啟佑 <wilsonwu@double-cash.com>
2024-03-07 14:36:03 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="h-20 border-b-[#DEE2EB] border-b">123</div>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
